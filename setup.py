de1082bdd954642ec2b52a0b4d3c95c0e2f5d45d
snbck <snbckcode@gmail.com>
2020-12-20 12:54:46 +0800

    feat: init

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 import setuptools
+import re
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("src/bracket/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
 setuptools.setup(
     name="bracket",
-    version="0.0.1",
+    version=version,
     author="Ceorleorn",
     author_email="snbck@qq.com",
     description="Bracket is an Elegant static site generator.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bracketing/bracket",
-    packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+        "Jinja2>=2.10.1"
+    ],
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
--- src/bracket/__init__.py
+++ src/bracket/__init__.py
@@ -1,5 +1,1 @@
-"""
-Bracket
-
-Elegant static site generator
-"""
+__version__ = "0.0.1"
