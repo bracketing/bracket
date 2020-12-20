c0d07734b6e80b734a0472c7b11ce5aef9b21d44
越努力越幸运✍ <snbckcode@gmail.com>
2020-12-20 06:14:24 +0000

    fixed: setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6'
+    ]
 
 )
